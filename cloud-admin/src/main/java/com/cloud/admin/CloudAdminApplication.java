3cdc82c8ceb710f1f548df977226b01ef7bb8c87
fangcy <1229837510@qq.com>
2022-10-06 09:41:15 +0800

    rabbitmq

--- cloud-admin/src/main/java/com/cloud/admin/CloudAdminApplication.java
+++ cloud-admin/src/main/java/com/cloud/admin/CloudAdminApplication.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication (
         scanBasePackages = {"com.cloud", "com.cloud.admin"}
         )
 @EnableTransactionManagement(proxyTargetClass = true)
+@EnableDiscoveryClient
 public class CloudAdminApplication {
     protected static final Logger logger = LoggerFactory.getLogger(CloudAdminApplication.class);
 
